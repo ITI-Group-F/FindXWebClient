48be58291465663c90de87001972faba893b1834
Mohamed Elsayed <odsh97@gmail.com>
2022-06-02 20:32:35 +0200

    Fixing layout and adding logo

--- src/components/layout/NavBar/Navbar.jsx
+++ src/components/layout/NavBar/Navbar.jsx
@@ -1,19 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.scss";
 export default function Navbar() {
   return (
 <>
 
-<p data-item='FindX'>FindX</p>
-<img src="" alt="" />
+{/* <p data-item='FindX'>FindX</p> */}
+<img src="img/logo.png" alt="" />
 <section>
   <nav>
     <ul class="menuItems">
-      <li><a href='#' data-item='Home'>Home</a></li>
-      <li><a href='#' data-item='About'>About</a></li>
-      <li><a href='#' data-item='Projects'>Projects</a></li>
-      <li><a href='#' data-item='Blog'>Blog</a></li>
-      <li><a href='#' data-item='Contact'>Contact</a></li>
+      <li><Link className="link" to='/' data-item='Home'>Home</Link></li>
+      <li><Link className="link" to='#' data-item='Login'>Login</Link></li>
+      <li><Link className="link" to='#' data-item='About'>About</Link></li>
+      <li><Link className="link" to='#' data-item='Contact'>Contact</Link></li>
     </ul>
   </nav>
 
