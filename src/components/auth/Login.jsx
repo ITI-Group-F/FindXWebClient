78c64a366f748014d95f4d17e58ac43b4d9926d5
Mohab Alnajjar <mohabsoft21@outlook.com>
2022-06-10 19:06:20 +0200

    Remake auth components

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,26 +1,44 @@
 import { Box, Button, FormControl, TextField, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 
 export default function Login() {
+  const [formData, setFormData] = useState({});
+
+  const setValue = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const submitLoginData = (event) => {
+    event.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <Box sx={{ display: "flex", flexDirection: "column" }}>
       <Typography variant="h1" component="div" gutterBottom>
         Login
       </Typography>
-      <form action="">
+      <form onSubmit={submitLoginData}>
         <TextField
           style={{ width: "200px", margin: "5px" }}
+          name="email"
           type="text"
           label="Email"
           variant="outlined"
+          onChange={setValue}
         />
         <TextField
           style={{ width: "200px", margin: "5px" }}
+          name="password"
           type="password"
           label="Password"
           variant="outlined"
+          onChange={setValue}
         />
-        <Button variant="contained">Login</Button>
+        <Button variant="contained" type="submit">
+          Login
+        </Button>
       </form>
     </Box>
   );
--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,5 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, Button, FormControl, TextField, Typography } from "@mui/material";
 
 export default function Register() {
-  return <div>Register</div>;
+  const [formData, setFormData] = useState(null);
+
+  const setValue = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const submitRegisterData = (event) => {
+    event.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column" }}>
+      <Typography variant="h1" component="div" gutterBottom>
+        Register
+      </Typography>
+      <form onSubmit={submitRegisterData}>
+        <TextField
+          style={{ width: "200px", margin: "5px" }}
+          name="firstName"
+          type="text"
+          label="First Name"
+          variant="outlined"
+          onChange={setValue}
+        />
+
+        <TextField
+          style={{ width: "200px", margin: "5px" }}
+          name="lastName"
+          type="text"
+          label="Last Name"
+          variant="outlined"
+          onChange={setValue}
+        />
+
+        <TextField
+          style={{ width: "200px", margin: "5px" }}
+          name="phone"
+          type="text"
+          label="Phone"
+          variant="outlined"
+          onChange={setValue}
+        />
+
+        <TextField
+          style={{ width: "200px", margin: "5px" }}
+          name="email"
+          type="text"
+          label="Email"
+          variant="outlined"
+          onChange={setValue}
+        />
+        <TextField
+          style={{ width: "200px", margin: "5px" }}
+          name="password"
+          type="password"
+          label="Password"
+          variant="outlined"
+          onChange={setValue}
+        />
+        <Button variant="contained" type="submit">
+          Register
+        </Button>
+      </form>
+    </Box>
+  );
 }
