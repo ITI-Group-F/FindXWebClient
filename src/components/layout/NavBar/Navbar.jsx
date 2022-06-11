aa57c95707d17fdd23d74868393afb8a63af736f
Mohamed Elsayed <odsh97@gmail.com>
2022-06-11 02:20:10 +0200

    adding Navbar mobile support

--- src/components/layout/NavBar/Navbar.jsx
+++ src/components/layout/NavBar/Navbar.jsx
@@ -1,28 +1,65 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.scss";
+
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
+
+import IconButton from '@mui/material/IconButton';
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+
+
 export default function Navbar() {
   return (
-<>
+    <>
 
-<div className=".xnav">
-<section>
-  <nav>
-    <ul class="menuItems">
-    <img src="img/logo.png" alt="" />
-      <li><Link className="link" to='/' data-item='Home'>Home</Link></li>
-      <li><Link className="link" to='/login' data-item='Login'>Login</Link></li>
-      <li><Link className="link" to='/register' data-item='Register'>Register</Link></li>
-      <li><Link className="link" to='#' data-item='Contact'>Contact</Link></li>
-    </ul>
-  </nav>
+      <div className=".xnav">
+        <section>
+          <nav>
+            <img src="img/logo.png" alt="" />
+            <ul className="menuItems">
+              <li><Link className="link" to='/' data-item='Home'>Home</Link></li>
+              <li><Link className="link" to='/login' data-item='Login'>Login</Link></li>
+              <li><Link className="link" to='/register' data-item='Register'>Register</Link></li>
+              <li><Link className="link" to='#' data-item='Contact'>Contact</Link></li>
+            </ul>
 
-</section>
-</div>
+        
+          <div className="HammButton" >
+            <PopupState variant="popover" popupId="hamburger">
+              {(popupState) => (
+                <React.Fragment>
+                  <IconButton aria-label="MenuRoundedIcon" color="secondary" {...bindTrigger(popupState)}>
+                  <MenuRoundedIcon />
+                  </IconButton>
+                  <Button variant="contained" {...bindTrigger(popupState)} >
+                   Manue
+                  </Button>
+
+                  <Menu {...bindMenu(popupState)}>
+                    <ul className="MobilMenuItems">
+
+                      <li><Link onClick={popupState.close} className="link" to='/' data-item='Home'>Home</Link></li>
+                      <li><Link onClick={popupState.close} className="link" to='/login' data-item='Login'>Login</Link></li>
+                      <li><Link onClick={popupState.close} className="link" to='/register' data-item='Register'>Register</Link></li>
+                      <li><Link onClick={popupState.close} className="link" to='#' data-item='Contact'>Contact</Link></li>
+
+                    </ul>
+                  </Menu>
+                </React.Fragment>
+              )}
+            </PopupState>
+            </div>
+          </nav>
 
 
-</>
+        </section>
+      </div>
 
-   
+
+    </>
+
+
   );
 }
