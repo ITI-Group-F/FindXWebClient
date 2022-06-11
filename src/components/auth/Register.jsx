52d302699ad178ac5105bb0d54bc759bea463f82
Mohab Alnajjar <mohabsoft21@outlook.com>
2022-06-11 14:18:02 +0200

    styling auth

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Box, Button, FormControl, TextField, Typography } from "@mui/material";
+import { Box, Button, Link, TextField, Typography } from "@mui/material";
+import "./auth.css";
 
 export default function Register() {
   const [formData, setFormData] = useState(null);
@@ -15,58 +16,91 @@ export default function Register() {
   };
 
   return (
-    <Box sx={{ display: "flex", flexDirection: "column" }}>
-      <Typography variant="h1" component="div" gutterBottom>
+    <>
+      <Typography variant="h3" component="div" gutterBottom>
         Register
       </Typography>
       <form onSubmit={submitRegisterData}>
-        <TextField
-          style={{ width: "200px", margin: "5px" }}
-          name="firstName"
-          type="text"
-          label="First Name"
-          variant="outlined"
-          onChange={setValue}
-        />
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            m: 1,
+            gap: 1,
+          }}
+        >
+          <Box>
+            <TextField
+              style={{ width: "200px", margin: "5px" }}
+              name="firstName"
+              type="text"
+              label="First Name"
+              variant="outlined"
+              onChange={setValue}
+            />
+          </Box>
+          <Box>
+            <TextField
+              style={{ width: "200px", margin: "5px" }}
+              name="lastName"
+              type="text"
+              label="Last Name"
+              variant="outlined"
+              onChange={setValue}
+            />
+          </Box>
+          <Box>
+            <TextField
+              style={{ width: "200px", margin: "5px" }}
+              name="phone"
+              type="text"
+              label="Phone"
+              variant="outlined"
+              onChange={setValue}
+            />
+          </Box>
+          <Box>
+            <TextField
+              style={{ width: "200px", margin: "5px" }}
+              name="email"
+              type="text"
+              label="Email"
+              variant="outlined"
+              onChange={setValue}
+            />
+          </Box>
+          <Box>
+            <TextField
+              style={{ width: "200px", margin: "5px" }}
+              name="password"
+              type="password"
+              label="Password"
+              variant="outlined"
+              onChange={setValue}
+            />
+          </Box>
+          <Box>
+            <Button
+              variant="contained"
+              type="submit"
+              className="sub-btn"
+              style={{ width: "200px", margin: "5px" }}
+            >
+              Register
+            </Button>
+          </Box>
+          <Box>
+            <Typography variant="subtitle2" component="span">
+              Already a member?
+            </Typography>
 
-        <TextField
-          style={{ width: "200px", margin: "5px" }}
-          name="lastName"
-          type="text"
-          label="Last Name"
-          variant="outlined"
-          onChange={setValue}
-        />
-
-        <TextField
-          style={{ width: "200px", margin: "5px" }}
-          name="phone"
-          type="text"
-          label="Phone"
-          variant="outlined"
-          onChange={setValue}
-        />
-
-        <TextField
-          style={{ width: "200px", margin: "5px" }}
-          name="email"
-          type="text"
-          label="Email"
-          variant="outlined"
-          onChange={setValue}
-        />
-        <TextField
-          style={{ width: "200px", margin: "5px" }}
-          name="password"
-          type="password"
-          label="Password"
-          variant="outlined"
-          onChange={setValue}
-        />
-        <Button variant="contained" type="submit">
-          Register
-        </Button>
+            <Link href="/register" style={{ marginLeft: 5 }}>
+              Login
+            </Link>
+          </Box>
+        </Box>
       </form>
-    </Box>
+    </>
   );
 }
--- src/components/layout/NavBar/Navbar.jsx
+++ src/components/layout/NavBar/Navbar.jsx
@@ -2,64 +2,109 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.scss";
 
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import PopupState, { bindTrigger, bindMenu } from "material-ui-popup-state";
 
-import IconButton from '@mui/material/IconButton';
-import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
-
+import IconButton from "@mui/material/IconButton";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 
 export default function Navbar() {
   return (
     <>
-
       <div className=".xnav">
         <section>
           <nav>
             <img src="img/logo.png" alt="" />
             <ul className="menuItems">
-              <li><Link className="link" to='/' data-item='Home'>Home</Link></li>
-              <li><Link className="link" to='/login' data-item='Login'>Login</Link></li>
-              <li><Link className="link" to='/register' data-item='Register'>Register</Link></li>
-              <li><Link className="link" to='#' data-item='Contact'>Contact</Link></li>
+              <li>
+                <Link className="link" to="/" data-item="Home">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link className="link" to="/login" data-item="Login">
+                  Login
+                </Link>
+              </li>
+              <li>
+                <Link className="link" to="/register" data-item="Register">
+                  Register
+                </Link>
+              </li>
+              <li>
+                <Link className="link" to="#" data-item="Contact">
+                  Contact
+                </Link>
+              </li>
             </ul>
 
-        
-          <div className="HammButton" >
-            <PopupState variant="popover" popupId="hamburger">
-              {(popupState) => (
-                <React.Fragment>
-                  <IconButton aria-label="MenuRoundedIcon" color="secondary" {...bindTrigger(popupState)}>
-                  <MenuRoundedIcon />
-                  </IconButton>
-                  <Button variant="contained" {...bindTrigger(popupState)} >
-                   Manue
-                  </Button>
+            <div className="HammButton">
+              <PopupState variant="popover" popupId="hamburger">
+                {(popupState) => (
+                  <React.Fragment>
+                    <IconButton
+                      aria-label="MenuRoundedIcon"
+                      color="secondary"
+                      {...bindTrigger(popupState)}
+                    >
+                      <MenuRoundedIcon />
+                    </IconButton>
+                    <Button variant="contained" {...bindTrigger(popupState)}>
+                      Manue
+                    </Button>
 
-                  <Menu {...bindMenu(popupState)}>
-                    <ul className="MobilMenuItems">
-
-                      <li><Link onClick={popupState.close} className="link" to='/' data-item='Home'>Home</Link></li>
-                      <li><Link onClick={popupState.close} className="link" to='/login' data-item='Login'>Login</Link></li>
-                      <li><Link onClick={popupState.close} className="link" to='/register' data-item='Register'>Register</Link></li>
-                      <li><Link onClick={popupState.close} className="link" to='#' data-item='Contact'>Contact</Link></li>
-
-                    </ul>
-                  </Menu>
-                </React.Fragment>
-              )}
-            </PopupState>
+                    <Menu {...bindMenu(popupState)}>
+                      <ul className="MobilMenuItems">
+                        <li>
+                          <Link
+                            onClick={popupState.close}
+                            className="link"
+                            to="/"
+                            data-item="Home"
+                          >
+                            Home
+                          </Link>
+                        </li>
+                        <li>
+                          <Link
+                            onClick={popupState.close}
+                            className="link"
+                            to="/login"
+                            data-item="Login"
+                          >
+                            Login
+                          </Link>
+                        </li>
+                        <li>
+                          <Link
+                            onClick={popupState.close}
+                            className="link"
+                            to="/register"
+                            data-item="Register"
+                          >
+                            Register
+                          </Link>
+                        </li>
+                        <li>
+                          <Link
+                            onClick={popupState.close}
+                            className="link"
+                            to="#"
+                            data-item="Contact"
+                          >
+                            Contact
+                          </Link>
+                        </li>
+                      </ul>
+                    </Menu>
+                  </React.Fragment>
+                )}
+              </PopupState>
             </div>
           </nav>
-
-
         </section>
       </div>
-
-
     </>
-
-
   );
 }
