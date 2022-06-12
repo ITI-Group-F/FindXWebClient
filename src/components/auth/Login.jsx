7315e1e488a08998d379caa67b96a1b25a875a33
Mohab Alnajjar <mohabsoft21@outlook.com>
2022-06-12 10:40:47 +0200

    login validation

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,18 +1,60 @@
-import { Box, Button, TextField, Typography, Link } from "@mui/material";
+import { Box, Button, TextField, Typography, Link, Alert } from "@mui/material";
 import React, { useState } from "react";
 import "./auth.css";
 
 export default function Login() {
   const [formData, setFormData] = useState({});
+  const [showMailErr, setShowMailErr] = useState(false);
+  const [showPassErr, setShowPassErr] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [mailErr, setMailErr] = useState("");
+  const [passErr, setPassErr] = useState("");
 
   const setValue = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
+    validateLogin();
+  };
+
+  const validateLogin = () => {
+    let isFormValid = false;
+    if (!formData.email || formData.email.length < 1) {
+      isFormValid = false;
+      setShowMailErr(true);
+      setMailErr("Email is required");
+    } else if (
+      formData.email &&
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(formData.email)
+    ) {
+      isFormValid = false;
+      setShowMailErr(true);
+      setMailErr("Invalid email address");
+    } else {
+      isFormValid = true;
+      setShowMailErr(false);
+    }
+
+    if (!isFormValid) return false;
+
+    if (!formData.password || formData.password.length < 1) {
+      isFormValid = false;
+      setShowPassErr(true);
+      setPassErr("Password is required");
+    } else {
+      isFormValid = true;
+      setShowPassErr(false);
+    }
+    return isFormValid;
   };
 
   const submitLoginData = (event) => {
     event.preventDefault();
-    console.log(formData);
+    if (!validateLogin()) {
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+      console.log(formData);
+    }
   };
 
   return (
@@ -30,6 +72,15 @@ export default function Login() {
             gap: 1,
           }}
         >
+          {showAlert && (
+            <center>
+              <Box width="300px">
+                <Alert severity="error">
+                  Some fileds are missing or invalid.
+                </Alert>
+              </Box>
+            </center>
+          )}
           <Box>
             <TextField
               style={{ width: "200px", margin: "5px" }}
@@ -40,6 +91,13 @@ export default function Login() {
               onChange={setValue}
             />
           </Box>
+          {showMailErr && (
+            <Box>
+              <Typography color="red" variant="caption" gutterBottom>
+                {mailErr}
+              </Typography>
+            </Box>
+          )}
           <Box>
             <TextField
               style={{ width: "200px", margin: "5px" }}
@@ -50,6 +108,13 @@ export default function Login() {
               onChange={setValue}
             />
           </Box>
+          {showPassErr && (
+            <Box>
+              <Typography color="red" variant="caption" gutterBottom>
+                {passErr}
+              </Typography>
+            </Box>
+          )}
           <Box>
             <Link href="#" style={{ marginLeft: 70 }}>
               Forgot password?
