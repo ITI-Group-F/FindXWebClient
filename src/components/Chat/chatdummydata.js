0c1cce3f3e89bd58a025100a0a21eb65984fccde
Mohamed Elsayed <odsh97@gmail.com>
2022-06-23 08:00:03 +0200

    adding populate contacts to chat app

---
+++ src/components/Chat/chatdummydata.js
@@ -0,0 +1,70 @@
+export let converstions = [
+  {
+    _id: "1",
+    sender: {
+        _id:"22",
+        FirstName:"Ahmed",
+        LastName:"Khaled"
+    },
+    receiver: {
+        _id:"22",
+        firstName:"Ahmed",
+        lastName:"Khaled"
+    },
+    messages: [
+      {
+        _id: "1",
+        content: "hello",
+        sendDate: "2022-06-19T11:11:27.861+00:00",
+        senderId: "",
+        seen: false,
+      },
+    ],
+  },
+
+  {
+    _id: "2",
+    sender: {
+        _id:"22",
+        FirstName:"Ahmed",
+        LastName:"Khaled"
+    },
+    receiver: {
+        _id:"22",
+        firstName:"Ahmed",
+        lastName:"Khaled"
+    },
+    messages: [
+      {
+        _id: "1",
+        content: "hello",
+        sendDate: "2022-06-19T11:11:27.861+00:00",
+        senderId: "",
+        seen: false,
+      },
+    ],
+  },
+
+  {
+    _id: "3",
+    sender: {
+        _id:"22",
+        FirstName:"Ahmed",
+        LastName:"Khaled"
+    },
+    receiver: {
+        _id:"22",
+        firstName:"Ahmed",
+        lastName:"Khaled"
+    },
+    messages: [
+      {
+        _id: "1",
+        content: "hello",
+        sendDate: "2022-06-19T11:11:27.861+00:00",
+        senderId: "",
+        seen: false,
+      },
+    ],
+  },
+];
