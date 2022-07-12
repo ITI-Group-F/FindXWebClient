f9a4b60cf647a02bec91fd46feb52e595a451d4c
Abdullah El-Zayat <abdullahelzayat23@gmail.com>
2022-07-12 16:02:19 +0200

    Fixed profile page UI container

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -165,12 +165,12 @@ export default function Profile(){
     }
 
      return(
-   <div className="container rounded bg-white mt-5 mb-5">
+   <div className="container profile-contanier rounded bg-white mt-5 mb-5">
     <div className="row">
         <div className="col-md-3 border-right">
             <div className="d-flex flex-column align-items-center text-center p-3 py-5"><img className="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"/><span className="font-weight-bold">{firstname+' '+lastname}</span><span className="text-black-50">{email}</span><span> </span></div>
         </div>
-        <div className="col-md-5 border-right">
+        <div className="col-md-9 border-right">
             <div className="p-3 py-5">
                 <div className="d-flex justify-content-between align-items-center mb-3">
                     <h4 className="text-right">Profile Settings</h4>
